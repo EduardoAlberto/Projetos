5b165d023cb8898727b3b064e3f764eef78b7fbc
Eduardo Alberto Andrade <33754720+EduardoAlberto@users.noreply.github.com>
2023-09-12 23:31:29 -0300

    alteração

--- data_engineer/scala/Spotify/src/main/scala/Classe_dados.scala
+++ data_engineer/scala/Spotify/src/main/scala/Classe_dados.scala
@@ -1,3 +1,39 @@
-class Classe_dados {
+import org.apache.spark.sql._
+import org.apache.log4j._
+import org.apache.spark
+import org.apache.spark.sql.functions._
+import java.util.Properties
+import org.apache.spark.sql.types.{DecimalType, FloatType, IntegerType, StringType, StructType}
+
+class dados(spark: SparkSession) {
+  def music(arq01: String): DataFrame = {
+    val df = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv(arq01)
+
+      df
+  }
 
 }
+
+object Script {
+  def main(args: Array[String]): Unit = {
+    //Logger.getLogger("org").setLevel(Level.ERROR)
+    val spark = SparkSession.builder()
+      .appName("Script de Carga")
+      .master("local")
+      .getOrCreate()
+
+    val TXT = new dados(spark)
+    val caminho = "/Users/eduardoalberto/LoadFile/data.csv"
+    val df = TXT.music(caminho)
+    df.show()
+
+    spark.stop()
+  }
+}
+
+
+
+
