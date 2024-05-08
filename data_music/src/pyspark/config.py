e9787f32220e280fe1c52739e8f40c587be4dda6
EduardoAlberto <eduardo.aandrad@gmail.com>
2024-05-07 23:19:01 -0300

    atualizacao

--- data_music/src/pyspark/config.py
+++ data_music/src/pyspark/config.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession,Row,DataFrame
 from run_etl import runJob
+from pyspark.sql.types import *
 
 # Create SparkSession
 spark=SparkSession.builder.master("local[2]")\
@@ -24,4 +25,15 @@ df_genres = x.genre()
 df_artists = x.artists()
 df_playlists = x.playlists()
 
-df_tracks.show()  
+df_music.write.format("delta").mode("overwrite").jdbc("jdbc:mysql://localhost:3306/myDbUser", "myDbUser.tb_music",properties={"user": "root", "password": "mysql"})
+df_tracks.write.format("delta").mode("overwrite").jdbc("jdbc:mysql://localhost:3306/myDbUser", "myDbUser.tb_tracks",properties={"user": "root", "password": "mysql"})
+df_genres.write.format("delta").mode("overwrite").jdbc("jdbc:mysql://localhost:3306/myDbUser", "myDbUser.tb_genres",properties={"user": "root", "password": "mysql"})
+df_artists.write.format("delta").mode("overwrite").jdbc("jdbc:mysql://localhost:3306/myDbUser", "myDbUser.tb_artists",properties={"user": "root", "password": "mysql"})
+df_playlists.write.format("delta").mode("overwrite").jdbc("jdbc:mysql://localhost:3306/myDbUser", "myDbUser.tb_playlists",properties={"user": "root", "password": "mysql"})
+
+print ("total:"+ str(df_music.count()))
+print ("total:"+ str(df_tracks.count()))
+print ("total:"+ str(df_genres.count()))
+print ("total:"+ str(df_artists.count()))
+print ("total:"+ str(df_playlists.count()))
+
