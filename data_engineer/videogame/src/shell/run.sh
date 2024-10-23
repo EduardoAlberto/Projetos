c673e8b63854e3c82d6adb1326d6fda8945a6a4d
Eduardo Alberto <eduardoalberto@MacBook-Pro-de-Eduardo.local>
2024-10-22 23:31:12 -0300

    criação do script shell

---
+++ data_engineer/videogame/src/shell/run.sh
@@ -0,0 +1,16 @@
+echo inicia carga 
+
+# Configurações do ambiente PySpark
+export SPARK_HOME=/Users/eduardoalberto/opt/spark-3.5.3-bin-hadoop3
+export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
+
+# Caminho para o script PySpark
+SCRIPT_PATH=/Users/eduardoalberto/Projetos/data_engineer/videogame/src/notebook/config.py
+
+# Comando para executar o script PySpark
+$SPARK_HOME/bin/spark-submit \
+  --master local[2] \
+  --name MySparkApp \
+  $SCRIPT_PATH
+
+echo carga finalida com sucesso
