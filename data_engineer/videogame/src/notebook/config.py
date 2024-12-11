c12fdf80063368f549b98faa72a4922a51c0dfd7
eduardo albertoe <eduardo.aandrad@gmail.com>
2024-12-10 22:25:28 -0300

    atualização o arguivos config

--- data_engineer/videogame/src/notebook/config.py
+++ data_engineer/videogame/src/notebook/config.py
@@ -4,7 +4,7 @@ from pyspark.sql.types import *
 from ingest import validaArquivo, carregaArquivo
 
 
-spark=SparkSession.builder.master("local")\
+spark=SparkSession.builder.master("yarn")\
         .appName("VideoGame")\
         .config("spark.driver.extraClassPath","/Users/eduardoalberto/opt/spark-3.5.3/jars/mysql-connector-j-8.2.0.jar" ) \
         .getOrCreate()
@@ -12,7 +12,7 @@ sc = spark.sparkContext
 spark.sparkContext.setLogLevel("OFF") 
 spark
 
-file = "/Users/eduardoalberto/LoadFile/video_games.csv"
+file = '/user/bigdata/repository/input/*.csv'
 df = validaArquivo(file,spark)
 print("TOTAL DE REGISTROS CARREGADOS: " + str(df.count()))
 carregaArquivo(file,spark)
