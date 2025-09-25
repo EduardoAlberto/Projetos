82986973b0c97865f1ab0c2e4a8c64674e5efecf
Eduardo Alberto <eduardoalberto@MacBook-Pro-de-Eduardo.local>
2025-09-24 22:47:04 -0300

    atualização dos novos script mongo

--- data_engineer/netflix/config/settings.py
+++ data_engineer/netflix/config/settings.py
@@ -6,6 +6,11 @@ class Config:
     SPARK_APP_NAME = "Netflix"
     SPARK_EXECUTOR_MEMORY = "2g"
 
+    # .CSV Configurações
+    CSV_OPTIONS = {
+        "arq": os.getenv("data_arq", "/Users/eduardoalberto/LoadFile/input/netflix_titles_clean.csv")
+    }
+
     # Fontes de dados
     DATA_SOURCES = {
         "jdbc_url": os.getenv("JDBC_URL", "jdbc:postgresql://localhost:5432/dbpostgres")
